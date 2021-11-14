e4bddd2fc3cb097ac15a5f426ec01f4a76cd66b6
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-11-14 13:49:46 -0300

    Added new functionalities to `window.sh`.

--- desktop/window.sh
+++ desktop/window.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/sh
 
-while getopts ":lr" option;
+while getopts ":lrMmf" option;
   do
     case $option in
       l)
         xdotool getactivewindow windowmove 0 0
         xdotool getactivewindow windowsize 679 y
-        wmctrl :ACTIVE: -b add,maximized_vert
+        wmctrl -r :ACTIVE: -b add,maximized_vert
         xdotool getactivewindow windowmove 2 32
       ;;
       r)
         xdotool getactivewindow windowmove 0 0
         xdotool getactivewindow windowsize 679 y
-        wmctrl :ACTIVE: -b add,maximized_vert
+        wmctrl -r :ACTIVE: -b add,maximized_vert
         xdotool getactivewindow windowmove 685 32
+      ;;
+      M)
+        wmctrl -r :ACTIVE: -b add,maximized_vert
+        wmctrl -r :ACTIVE: -b add,maximized_horz
+      ;;
+      m)
+        wmctrl -r :ACTIVE: -b remove,maximized_vert
+        wmctrl -r :ACTIVE: -b remove,maximized_horz
+      ;;
+      f)
+        wmctrl -r :ACTIVE: -b toggle,fullscreen
       ;;
       *)
         exit 1
