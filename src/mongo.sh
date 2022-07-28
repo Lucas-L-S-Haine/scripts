51f7e81c2ba2c220a22868ee9bee3b3319d6ad2b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-27 22:27:00 -0300

    chore: delete scripts

--- src/mongo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ -t 0 ]; then
-	docker container exec -it mongo mongosh --quiet "$@"
-else
-	docker container exec -i mongo mongosh --quiet "$@"
-	printf '\n'
-fi
--- src/mysql.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ -t 0 ]; then
-	docker container exec -it mysql mysql "$@"
-else
-	docker container exec -i mysql mysql "$@"
-	printf '\n'
-fi
