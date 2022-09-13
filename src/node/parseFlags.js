5aa9d2cdaada83e03a656e32fd11b5834fb2cf0d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-13 00:29:39 -0300

    revert: remove shelljs from dependencies

--- src/node/parseFlags.js
+++ src/node/parseFlags.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const { _: args, ...options } = require('minimist')(process.argv.slice(2));
-const { exec } = require('shelljs');
 
 console.log('args:', args);
 console.log('options:', options);
