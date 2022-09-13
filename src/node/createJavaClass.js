64365feff53684209f83171fe37c4c261f4e44c0
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-12 23:10:23 -0300

    feat: create new java class files

---
+++ src/node/createJavaClass.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const { basename } = require('path');
+const { _: classes, ...options } = require('minimist')(process.argv.slice(2));
+
+const dirname = basename(__dirname);
+
+const packageName = options.package || options.p || dirname;
+const code = (packageName, className) =>
+  `package ${packageName};
+
+public class ${className} {
+
+}\n`;
+
+classes
+  .map((className) => [`${className}.java`, className])
+  .forEach(([file, className]) => fs.writeFileSync(file, code(packageName, className),
+    { encoding: 'utf-8', mode: 0o644, flag: 'w' }))
