f63bc53e6e06169e5f11fd0181eac966b44a7937
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-07 15:45:49 -0300

    refactor: move lines of code into try statement

--- src/python/update_clock.py
+++ src/python/update_clock.py
@@ -8,16 +8,16 @@ from dateutil.relativedelta import relativedelta
 
 try:
     datetime = request("head", "http://www.google.com").headers["date"]
+
+    localtime = parse(datetime) + relativedelta(hours=-3)
+    time_str = f"{localtime.date()} {localtime.time()}"
+
+    run(["doas", "timedatectl", "set-ntp", "false"])
+    time.sleep(1)
+    run(["doas", "timedatectl", "set-time", time_str])
+    time.sleep(1)
+    run(["doas", "timedatectl", "set-ntp", "true"])
 except BaseException as error:
     run(["notify-send", "--expire-time=10000", "Error: failed to update clock"])
     run(["notify-send", "--expire-time=10000", error.__str__()])
     sys.exit(1)
-
-localtime = parse(datetime) + relativedelta(hours=-3)
-time_str = f"{localtime.date()} {localtime.time()}"
-
-run(["doas", "timedatectl", "set-ntp", "false"])
-time.sleep(1)
-run(["doas", "timedatectl", "set-time", time_str])
-time.sleep(1)
-run(["doas", "timedatectl", "set-ntp", "true"])
