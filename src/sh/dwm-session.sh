07040ea792dea54b90ea4a5cdc880a3c7c07a6de
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-12 23:33:07 -0300

    revert: delete more scripts

--- src/sh/dwm-session.sh
+++
@@ -1,7 +0,0 @@
-while true; do
-	status="$(dwm-status-bar)"
-	xsetroot -name "${status}"
-	sleep 5
-done &
-
-exec dwm
--- src/sh/send-json.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-curl \
-    -d "${1}" \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    "${2}"
