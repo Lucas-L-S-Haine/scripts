cfd82f8af97ef44cfb8cb136054bda4862f269e7
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-18 09:10:08 -0300

    feat: add non-interactive mode flag

--- src/python/update_clock.py
+++ src/python/update_clock.py
@@ -12,11 +12,11 @@ try:
     localtime = parse(datetime) + relativedelta(hours=-3)
     time_str = f"{localtime.date()} {localtime.time()}"
 
-    run(["doas", "timedatectl", "set-ntp", "false"])
+    run(["doas", "-n", "timedatectl", "set-ntp", "false"])
     time.sleep(1)
-    run(["doas", "timedatectl", "set-time", time_str])
+    run(["doas", "-n", "timedatectl", "set-time", time_str])
     time.sleep(1)
-    run(["doas", "timedatectl", "set-ntp", "true"])
+    run(["doas", "-n", "timedatectl", "set-ntp", "true"])
 except BaseException as error:
     run(["notify-send", "--expire-time=10000", "Error: failed to update clock"])
     run(["notify-send", "--expire-time=10000", error.__str__()])
