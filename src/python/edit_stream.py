3a7b5c663293b5e38c32621fdb24221c3f237f45
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-18 14:28:30 -0300

    feat: edit stdin and send result to stdout

---
+++ src/python/edit_stream.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import sys
+import tempfile
+from subprocess import run
+
+
+try:
+    exit_status = 0
+    tmp_file = tempfile.NamedTemporaryFile(mode="w", delete=False).file
+
+    if not sys.stdin.isatty():
+        file = open(tmp_file.name, mode="w")
+        file.write(sys.stdin.read())
+        file.close()
+
+    file = open(tmp_file.name, mode="r")
+
+    if not sys.stderr.isatty():
+        raise RuntimeError("Error: this script doesn't allow stderr redirection")
+
+    run(["nvim", file.name], stdout=sys.stderr)
+
+    sys.stdout.write(file.read())
+
+except BaseException as error:
+    print(error)
+    exit_status = 1
+
+finally:
+    file.close()
+    os.remove(tmp_file.name)
+    sys.exit(exit_status)
