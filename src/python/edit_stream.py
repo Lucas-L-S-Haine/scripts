75585ee8dbb6669bd4b6129f193f344557895e4e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-22 17:12:22 -0300

    revert: remove error treatment

--- src/python/edit_stream.py
+++ src/python/edit_stream.py
@@ -8,10 +8,7 @@ from subprocess import run
 def main():
     """Reads input from stdin, edits on neovim and pipes into stdout"""
 
-    ERROR_MESSAGE = "Error: this script doesn't allow stderr redirection"
-
     try:
-        exit_status = 0
         input_tty = open("/dev/tty", mode="r")
         output_tty = open("/dev/tty", mode="w")
         tmp_file = tempfile.NamedTemporaryFile(mode="w", delete=False).file
@@ -22,21 +19,13 @@ def main():
 
         file = open(tmp_file.name, mode="r")
 
-        if not sys.stderr.isatty():
-            raise RuntimeError(ERROR_MESSAGE)
-
         run(["nvim", file.name], stdin=input_tty, stdout=output_tty)
 
         sys.stdout.write(file.read())
 
-    except BaseException as error:
-        print(error)
-        exit_status = 1
-
     finally:
         file.close()
         os.remove(tmp_file.name)
-        sys.exit(exit_status)
 
 
 if __name__ == "__main__":
