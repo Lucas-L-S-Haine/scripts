506debb789eb9ff50b5d9f737c6c3b1046427f38
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-02-28 17:25:35 -0300

    chore: create script for handling toml format

---
+++ src/python/toml.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+import toml
+
+
+def read_toml():
+    pass
+
+
+def write_toml():
+    pass
+
+
+main():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
