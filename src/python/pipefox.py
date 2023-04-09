eb7e77af51065d6aeecaa93e3dc205d1203a4405
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-09 17:30:28 -0300

    feat: add docopt module
    Use docopt to parse command line arguments, instead of argparse.

--- src/python/pipefox.py
+++ src/python/pipefox.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python3
+"""Pipefox - A simple script to pipe html into your browser
+
+Usage:
+    pipefox.py [--help | -h]
+    pipefox.py [--format=<format> | -f <format>] [<browser>] [<file> | -]
+
+Positional Arguments:
+    browser                The browser to open the html page
+    file                   The file from which the html content will be read.
+                           If <file> is "-", then pipefox.py will read from
+                           stdin.
+
+Options:
+    -h, --help             show this help message and exit
+    -f, --format=<format>  the format to use for the file [default: html]
+"""
 import os
 import sys
 import tempfile as tmp
 import subprocess as sp
-import argparse
+import io
+
+from docopt import docopt
 
 
-BROWSER = os.environ.get("BROWSER", "x-www-browser")
+options = docopt(__doc__)
+
+browser = options["<browser>"] or os.environ.get("BROWSER", "x-www-browser")
+should_read_from_tty = options["<file>"] == "-" or options["<file>"] is None
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("browser", nargs="?", default=BROWSER)
+file = io.TextIOBase()
+try:
+    if should_read_from_tty:
+        file = sys.stdin
+    else:
+        file = open(options["<file>"], mode="r")
 
-args = parser.parse_args()
+    with tmp.NamedTemporaryFile(suffix=".html") as tmp_file:
+        tmp_file.write(file.read().encode())
 
+        sp.run([browser, tmp_file.name], input=tmp_file.read())
 
-with tmp.NamedTemporaryFile(suffix=".html") as tmp_file:
-    tmp_file.write(sys.stdin.read().encode())
-
-    sp.run([args.browser, tmp_file.name], input=tmp_file.read())
+finally:
+    file.close()
