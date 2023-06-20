d6f206e9f0bdd6cd8768b6c9f2076e9bdf961311
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-20 20:07:20 -0300

    feat: add preprocessor constants

--- src/c/symlink.c
+++ src/c/symlink.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define ERROR_INSUFICIENT_ARGUMENTS 1
+#define ERROR_SYMLINK_CREATION 3
+
 int file_exists(char *filepath) {
 	int result = faccessat(AT_FDCWD, filepath, F_OK, AT_SYMLINK_NOFOLLOW) + 1;
 	return result;
@@ -10,7 +13,7 @@ int file_exists(char *filepath) {
 int main(int argc, char **argv) {
 	if (argc != 3) {
 		fprintf(stderr, "Error: the %s command requires two arguments.\n", argv[0]);
-		return 1;
+		return ERROR_INSUFICIENT_ARGUMENTS;
 	}
 
 	char *target = argv[1];
@@ -18,7 +21,7 @@ int main(int argc, char **argv) {
 
 	if (file_exists(link_name)) {
 		fprintf(stderr, "Error: the file %s already exists.\n", link_name);
-		return 3;
+		return ERROR_SYMLINK_CREATION;
 	}
 
 	symlink(target, link_name);
