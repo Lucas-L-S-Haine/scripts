d6a69ec9e17153067d34564da6ffb3f36da4f9e5
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-06-24 17:05:37 -0300

    refactor: replace pulsemixer with amixer

--- src/sh/volume.sh
+++ src/sh/volume.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 urgency() {
-  left_volume=$(pulsemixer --get-volume | awk '{print $1}')
-  right_volume=$(pulsemixer --get-volume | awk '{print $2}')
+  left_volume=$(amixer get Master | grep -oE "[0-9]+%" | head -n 1 | tr -d '%')
+  right_volume=$(amixer get Master | grep -oE "[0-9]+%" | tail -n 1 | tr -d '%')
 
   if test "${left_volume}" -gt 100 -o "${right_volume}" -gt 100; then
     printf '%s' critical
@@ -11,15 +11,14 @@ urgency() {
   fi
 }
 
-
 if test "$1" = increase; then
-  pulsemixer --change-volume +${2:-2} --max-volume 150
-  dunstify volume "$(pulsemixer --get-volume)" \
-    --replace=1 --timeout=2100 -u $(urgency)
+  amixer set Master "${2:-2}"%+ unmute
+  dunstify volume "$(amixer get Master | awk '/[0-9]+%/ {print $5}')" \
+    --replace=1 --timeout=2100 -u "$(urgency)"
 elif test "$1" = decrease; then
-  pulsemixer --change-volume -${2:-2}
-  dunstify volume "$(pulsemixer --get-volume)" \
-    --replace=1 --timeout=2100 -u $(urgency)
+  amixer set Master "${2:-2}"%- unmute
+  dunstify volume "$(amixer get Master | awk '/[0-9]+%/ {print $5}')" \
+    --replace=1 --timeout=2100 -u "$(urgency)"
 elif test -z "$1"; then
-  pulsemixer --get-volume
+  amixer get Master | grep -oE "[0-9]+%"
 fi
