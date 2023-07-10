62f1b786205f0ff1dce254c5c8d38ca667ac80a4
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-10 04:37:17 -0300

    feat: add example of lib usage

---
+++ src/c/main.c
@@ -0,0 +1,11 @@
+#include "lib/run.h"
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main() {
+	int status;
+	char *argv[] = {"notify-send", "message", "Hello, World!", NULL};
+	runfg("/usr/bin/notify-send", argv, &status);
+
+	return 0;
+}
