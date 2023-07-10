b786bec62e0d8aeb2aee7c3a8df3d24eb6efd025
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-10 11:26:49 -0300

    feat: create a xinitrc out of main.c

--- src/c/lib/main.c
+++ src/c/lib/main.c
@@ -1,11 +1,42 @@
 #include "run.h"
 #include <stdio.h>
 #include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
 
 int main() {
-	int status;
-	char *argv[] = {"notify-send", "message", "Hello, World!", NULL};
-	runfg("/usr/bin/notify-send", argv, &status);
+	char *picom[] = {"picom", NULL}; runbg("picom", picom);
+	char *setxkbmap[] = {"setxkbmap", NULL}; runbg("setxkbmap", setxkbmap);
+	char *numlockx[] = {"numlockx", "on", NULL}; runbg("numlockx", numlockx);
+
+	char *wallpaper = getenv("WALLPAPER");
+	if (wallpaper != NULL) {
+		char *feh[] = {"feh", "--bg-scale", wallpaper, NULL};
+		runbg("feh", feh);
+	}
+
+	char *dunst[] = {"dunst", NULL}; runbg("dunst", dunst);
+
+	char *xset1[] = {"xset", "dpms", "0", "0", "0", NULL}; runbg("xset", xset1);
+	char *xset2[] = {"xset", "s", "off", "-dpms", NULL}; runbg("xset", xset2);
+
+	char *bar[] = {"dwm-status-bar", NULL}; runbg("dwm-status-bar", bar);
+
+	char *tty = ttyname(STDIN_FILENO);
+	char *dwm[] = {"dwm", NULL};
+	if (tty != NULL && strcmp(tty, "/dev/tty1") == 0) {
+		runfg("dwm", dwm, NULL);
+
+		char *fish_pid_str = getenv("FISH_PID");
+		if (fish_pid_str != NULL) {
+			int fish_pid = atoi(fish_pid_str);
+			kill(fish_pid, SIGHUP);
+		}
+	} else {
+		execv("/usr/local/bin/dwm", dwm);
+	}
 
 	return 0;
 }
