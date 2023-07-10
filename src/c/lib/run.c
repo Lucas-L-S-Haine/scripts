db8a905b0573a878d945a3a604bc6860f44eaab9
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-07-10 04:36:31 -0300

    feat: add library with run functions

---
+++ src/c/lib/run.c
@@ -0,0 +1,38 @@
+#include "run.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+
+int runbg(char *command, char *argv[]) {
+	pid_t child_pid = fork();
+
+	if (child_pid == 0) {
+		execvp(command, argv);
+		perror("execvp failed");
+		return -1;
+	} else if (child_pid < 0) {
+		fprintf(stderr, "Error: failed to run %s\n", command);
+		return -1;
+	}
+
+	return child_pid;
+}
+
+int runfg(char *command, char *argv[], int *status_ptr) {
+	pid_t child_pid = fork();
+
+	if (child_pid == 0) {
+		execvp(command, argv);
+		perror("execvp failed");
+		return -1;
+	} else if (child_pid < 0) {
+		fprintf(stderr, "Error: failed to run %s\n", command);
+		return -1;
+	} else {
+		waitpid(child_pid, status_ptr, 0);
+	}
+
+	return child_pid;
+}
---
+++ src/c/lib/run.h
@@ -0,0 +1,8 @@
+#ifndef RUNFG_H
+#define RUNFG_H
+
+int runbg(char *command, char *argv[]);
+
+int runfg(char *command, char *argv[], int *status_ptr);
+
+#endif
