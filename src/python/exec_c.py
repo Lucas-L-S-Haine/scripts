805b09313cfaedbc8b18359d82f4dde68a805ac2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-10-20 21:08:30 -0300

    feat: add ldflags and arguments
    Add support for linking flags and commandline arguments passed directly
    to the compiled program.

--- src/python/exec_c.py
+++ src/python/exec_c.py
@@ -1,59 +1,152 @@
 #!/usr/bin/env python3
-"""exec-c - Compile and execute C code
+"""Compile one or more C source code files into a temporary binary file and
+execute it immediately.
 
-Usage:
-    exec-c (-h | --help)
-    exec-c [-c <compiler> | --cc=<compiler> | --compiler=<compiler>]
-           [-f <flags> | --flags=<flags>]... <src_file> [<args>]...
-
-Options:
-    -h, --help              show this help message
-    -c, --cc=<compiler>     choose a different compiler[default: gcc]
-    --compiler=<compiler>   alias for --cc
-    -f, --flags=<flags>     specify the compiler flags[default: -Wall -Wextra -Werror]
-
-Arguments:
-    src_file    source code written in C
-    args        command-line arguments
+The script uses its own options to choose the compiler, the compiler flags and
+the linking flags, along with source code files. If you want to pass arguments
+directly to the compiled program, you can do so by passing "--" to the
+commandline, followed by the desired arguments.
 """
 import os
 import sys
 import subprocess as sp
 import tempfile as tmp
+from getopt import GetoptError, gnu_getopt as getopt
 
-from docopt import docopt
+
+SHORT_OPTS = "hc:f:l:n"
+LONG_OPTS  = ["help", "cc=", "cflags=", "ldflags=", "compiler=", "flags=",
+              "dry-run"]
 
 
-options = docopt(__doc__)
-if options["--compiler"] is None:
-    options["--compiler"] = options["--cc"]
-else:
-    options["--cc"] = options["--compiler"]
+CC      = os.environ.get("CC", "cc")
+CFLAGS  = os.environ.get("CFLAGS", "-Wall:-Werror:-Wextra")
+LDFLAGS = os.environ.get("LDFLAGS", "")
 
-CC = options["--compiler"]
-CFLAGS = list(set(options["--flags"]))
 
-src_file = options["<src_file>"]
-argv = options["<args>"]
+def separate_arguments(argv):
+    """Separate list in two by searching for a "--" argument."""
+    try:
+        index = argv.index("--")
+        return argv[:index], argv[index + 1:]
+    except ValueError:
+        return argv, []
+
+
+def print_help():
+    """Print help message."""
+    message = f"""Usage:
+    {sys.argv[0]} (-h | --help)
+    {sys.argv[0]} [-c <compiler> | --cc=<compiler> | --compiler=<compiler>]
+           [-f <flags> | --flags=<flags>]... <src_file>... [--] [<arg>]...
+
+Options:
+    -h, --help               show this help message
+    -c, --cc=<compiler>      choose a different compiler [default: cc]
+    -f, --cflags=<flags>     specify the compiler flags [default: -Wall -Wextra -Werror]
+    -l, --ldflags=<flags>    specify linking flags
+
+Arguments:
+    src_file    source code written in C
+    arg         command-line arguments"""
+    print(message)
+    sys.exit(0)
+
+
+def dry_run(options, files, arguments):
+    """Simulate the program's execution."""
+    compiler = options["compiler"]
+    cflags   = options["cflags"]
+    ldflags  = options["ldflags"]
+
+    output_file = tmp.NamedTemporaryFile().name
+
+    compile_program = [compiler, *cflags, "-o", output_file, *files]
+    execute_program = [output_file, *arguments]
+    print(*get_truthy_values(compile_program))
+    print(*get_truthy_values(execute_program))
+    sys.exit(0)
+
+
+def get_compiler_options(option_list, **kwargs):
+    """Organize compiler options in a dictionary."""
+    options = kwargs
+    options["dry-run"] = False
+
+    for flag, value in option_list:
+        if flag in ["-h", "--help"]:
+            print_help()
+        elif flag in ["-c", "--cc", "--compiler"]:
+            options["compiler"] = value
+        elif flag in ["-f", "--cflags", "--flags"]:
+            options["cflags"] = value.split()
+        elif flag in ["-l", "--ldflags"]:
+            options["ldflags"] = value.split()
+        elif flag in ["-n", "--dry-run"]:
+            options["dry-run"] = True
+
+    return options
+
+
+def parse_commandline_arguments(argv=sys.argv[1:]):
+    """Parse all options and arguments necessary for the program's execution."""
+    compiler_args, arguments = separate_arguments(argv)
+
+    try:
+        options, files = getopt(compiler_args, SHORT_OPTS, LONG_OPTS)
+    except GetoptError as e:
+        print("Error:", e.msg, file=sys.stderr)
+        sys.exit(2)
+
+    options = get_compiler_options(
+        options, compiler=CC, cflags=CFLAGS.split(":"),
+        ldflags=LDFLAGS.split(":")
+    )
+
+    return options, files, arguments
+
+
+def get_truthy_values(value_list):
+    """Return a copy of a list containing only truthy values."""
+    values = []
+    for value in value_list:
+        if value:
+            values.append(value)
+
+    return values
 
 
 def main():
-    if src_file is None:
-        print("Error: no file provided", file=sys.stderr)
-        sys.exit(5)
+    options, files, arguments = parse_commandline_arguments()
 
+    compiler = options["compiler"]
+    cflags   = options["cflags"]
+    ldflags  = options["ldflags"]
 
-    file = tmp.NamedTemporaryFile().name
-    status = 0
+    if options["dry-run"]:
+        dry_run(options, files, arguments)
+
+    if not files:
+        print("Error: no filename was provided", file=sys.stderr)
+        return 5
+
+    status = 1
     try:
-        proc = sp.run([CC, *CFLAGS, "--output", file, src_file])
+        output_file = tmp.NamedTemporaryFile().name
+        command = [compiler, *cflags, "-o", output_file, *files, *ldflags]
+        command = get_truthy_values(command)
+        proc = sp.run(command)
         status = proc.returncode
         if status == 0:
-            proc = sp.run([file, *argv])
+            command = get_truthy_values([output_file, *arguments])
+            proc = sp.run(command)
             status = proc.returncode
+    except Exception as e:
+        print("Error:", e.args[0], file=sys.stderr)
+        status = 1
     finally:
-        if os.access(file, os.F_OK):
-            os.unlink(file)
+        if os.access(output_file, os.F_OK):
+            os.unlink(output_file)
 
         return status
 
