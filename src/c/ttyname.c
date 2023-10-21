49d7bc91d39e5ee751bc5d800455a395a2c1b79c
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-10-21 00:34:52 -0300

    feat: allow custom file descriptors

--- src/c/ttyname.c
+++ src/c/ttyname.c
@@ -1,8 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	char *tty = ttyname(STDIN_FILENO);
+int main(int argc, char *argv[]) {
+	int fd;
+	if (argc < 2) {
+		fd = 0;
+	} else {
+		fd = atoi(argv[1]);
+	}
+
+	if (!isatty(fd)) {
+		fprintf(stderr,
+				"Error: file descriptor %d is not connected to a tty\n", fd);
+		return 1;
+	}
+
+	char *tty = ttyname(fd);
 	printf("%s\n", tty);
 	return 0;
 }
