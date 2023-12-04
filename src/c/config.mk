73a9a692cee87b451fb3058a100701db305c9b4f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-12-03 21:05:05 -0300

    chore: update notify-me rule

---
+++ src/c/config.mk
@@ -0,0 +1,24 @@
+ID != awk -F= -e '/^ID/ { print $$2 }' /etc/os-release
+
+ifeq ($(ID),artix)
+	ifeq ($(filter notify-me,$(MAKECMDGOALS)),notify-me)
+		NEW_CFLAGS = -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -I/usr/include/gdk-pixbuf-2.0 -pthread
+		TEMP := $(CFLAGS)
+		override CFLAGS += $(filter-out $(TEMP),$(NEW_CFLAGS))
+		TEMP := $(LDFLAGS)
+		override LDFLAGS += $(filter-out $(TEMP),-lnotify -lgobject-2.0)
+	endif
+endif
+
+ifeq ($(ID),debian)
+	ifeq ($(filter notify-me,$(MAKECMDGOALS)),notify-me)
+		NEW_CFLAGS = -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/gdk-pixbuf-2.0 -pthread
+		TEMP := $(CFLAGS)
+		override CFLAGS += $(filter-out $(TEMP),$(NEW_CFLAGS))
+		TEMP := $(LDFLAGS)
+		override LDFLAGS += $(filter-out $(TEMP),-lnotify -lgobject-2.0)
+	endif
+endif
+
+notify-me: notify_me.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
